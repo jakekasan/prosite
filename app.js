56a4efb13f9cc31e86c67cb2b450a4ab862f7f6d
jakekasan <jake.kasan@gmail.com>
2018-07-21 14:37:28 +0100

    web up and running

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+//const bodyParser = require("body-parser");
+
+//app.use(bodyParser);
+
+app.use(express.static("./static"));
+
+// app.use("/",(req,res) => {
+//     console.log("GET '/'");
+//     res.send("hello");
+// })
 
 app.listen(8080, () => {
     console.log("Running on 8080");
-})
+});
